cdd0c0fd0d2e59311e7ff2350d828eeae2cc36de
asmaraldalf <asmaraldalf@gmail.com>
2022-09-12 01:27:37 +0700

    initial commit

---
+++ ClientLoginInfo/ClientLoginInfo/client.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Project_Login_Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Console.WriteLine("Enter your name: ");
+            person.Name = Console.ReadLine();
+            Console.WriteLine("Enter your email address: ");
+            person.Email = Console.ReadLine();
+
+            byte[] bytes = sendMessage(System.Text.Encoding.Unicode.GetBytes(person.ToJson()));
+            Console.WriteLine(cleanMessage(bytes));
+
+            Console.Read();
+        }
+
+        private static byte[] sendMessage(byte[] messageBytes)
+        {
+            const int bytesize = 1024 * 1024;
+            try
+            {
+                System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient("127.0.0.1", 4141);
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(messageBytes, 0, messageBytes.Length);
+
+                Console.WriteLine("================================");
+                Console.WriteLine("=   Connected to the server    =");
+                Console.WriteLine("================================");
+                Console.WriteLine("Waiting for response...");
+
+                messageBytes = new byte[bytesize];
+
+                stream.Read(messageBytes, 0, messageBytes.Length);
+
+                stream.Dispose();
+                stream.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return messageBytes;
+        }
+
+        private static string cleanMessage(byte[] bytes)
+        {
+            string message = System.Text.Encoding.Unicode.GetString(bytes);
+            Console.WriteLine(message);
+
+            string messageToPrint = null;
+            foreach (var nullChar in message)
+            {
+                if (nullChar != '\0')
+                {
+                    messageToPrint += nullChar;
+                }
+            }
+
+            return messageToPrint;
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public string ToJson()
+        {
+            string data = "{";
+            data += "'name': '" + Name + "',";
+            data += "'email': '" + Email + "',";
+            data += "}";
+            return data;
+        }
+    }
+}
---
+++ ServerLoginInfo/ServerLoginInfo/server.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Mail;
+using Newtonsoft.Json;
+
+namespace Project_Login
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 4141);
+            TcpListener listener = new TcpListener(endPoint);
+            listener.Start();
+
+            Console.WriteLine(@"Server Started /n at {0}:{1}", endPoint.Address, endPoint.Port);
+
+            while (true)
+            {
+                const int bytesize = 1024 * 1024;
+
+                string message = null;
+                byte[] buffer = new byte[bytesize];
+
+                var sender = listener.AcceptTcpClient();
+                sender.GetStream().Read(buffer, 0, bytesize);
+
+                message = cleanMessage(buffer);
+
+                Person person = JsonConvert.DeserializeObject<Person>(message);
+
+                byte[] bytes = System.Text.Encoding.Unicode.GetBytes("Your message was sent, " + person.Name);
+                sender.GetStream().Write(bytes, 0, bytes.Length);
+
+                sendEmail(person);
+            }
+        }
+
+        private static void sendEmail(Person p)
+        {
+            try
+            {
+                using (SmtpClient client = new SmtpClient("<smtp-server>", 25))
+                {
+                    client.EnableSsl = true;
+                    client.Credentials = new NetworkCredential("<email-address>", "<pass>");
+
+                    client.Send(
+                        new MailMessage("<your-email>", p.Email,
+                            "Thank you for using this Services",
+                            string.Format(@"Thank you for using this Services, {0}.   
+                                We have recieved your message, '{1}'.", p.Name
+                            )
+                        )
+                    );
+
+                    Console.WriteLine("Email sent to " + p.Email);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static string cleanMessage(byte[] bytes)
+        {
+            string message = System.Text.Encoding.Unicode.GetString(bytes);
+
+            string messageToPrint = null;
+            foreach (var nullChar in message)
+            {
+                if (nullChar != 0)
+                {
+                    messageToPrint += nullChar;
+                }
+            }
+
+            return messageToPrint;
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        
+    }
+}
